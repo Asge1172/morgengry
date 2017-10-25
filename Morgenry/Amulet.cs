9980870799eccb9ec04b9757397ab798a51070df
Asge1172 <32439265+Asge1172@users.noreply.github.com>
2017-10-25 08:50:25 +0200

    Add files via upload

---
+++ Morgenry/Amulet.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgenry
+{
+
+    public enum Level
+    {
+        low,
+        medium,
+        high
+    };
+   public class Amulet
+    { //Property
+        public string ItemId { get; set; }
+
+        public string Design { get; set; }
+
+        public Level Quality { get; set; }
+
+        public Amulet(string itemId, Level quality, string design)
+        {
+            ItemId = itemId;
+            Quality = quality;
+            Design = design;
+        }
+
+
+        public Amulet(string itemId) :
+            this (itemId, Level.medium, "")
+        {
+        }
+
+        public Amulet (string itemId, Level quality) :
+            this (itemId, quality, "")
+        {
+        }
+
+        public Amulet () :
+            this ("", Level.medium, "")
+        {
+        }
+
+        public override string ToString() { return "ItemId: " + ItemId + ", Quality: " + Quality + ", Design: " + Design; }
+    }
+}
---
+++ Morgenry/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgenry
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public double Price { get; set; }
+
+        public string ItemId { get; set; }
+
+
+        public Book(string itemId, string title, double price)
+        {
+            ItemId = itemId;
+            Title = title;
+            Price = price;
+        }
+
+        public Book (string itemId) :
+            this (itemId, "", 0)
+        {
+        }
+
+        public Book(string itemId, string title) :
+            this (itemId, title, 0)
+        {  
+        }
+
+        public Book () :
+            this ("", "", 0)
+        {
+        }
+
+        public override string ToString() { return "ItemId: " + ItemId + ", Title: " + Title + ", Price: " + Price; }
+
+    }
+}
