8288d7906370a8d2db2299955620a4b1fcc8392f
Asge1172 <32439265+Asge1172@users.noreply.github.com>
2017-10-25 08:46:56 +0200

    Add files via upload

---
+++ Morgengry/Amulet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public enum Level { low, medium, high };
+
+namespace Morgengry
+{
+    public class Amulet
+    {
+        string ItemId { get; set; }
+        string Design { get; set; }
+        Level Quality { get; set; }
+
+        public Amulet(string _itemId, Level _quality, string _design)
+        {
+            ItemId = _itemId;
+            Quality = _quality;
+            Design = _design;
+        }
+        public Amulet(string _itemId, Level _quality) :
+            this(_itemId, _quality, string.Empty)
+        {
+        }
+        public Amulet(string _itemId) :
+            this(_itemId, Level.medium, string.Empty)
+        {
+            ItemId = _itemId;
+        }
+        
+        public override string ToString()
+        {
+            return "ItemId: " + ItemId + ", Quality: " + Quality.ToString() + ", Design: " + Design;
+        }
+        
+        
+
+        
+    }
+}
---
+++ Morgengry/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public class Book
+    {
+        
+        string ItemId { get; set; }
+        
+        string Title { get; set; }
+        
+        double Price { get; set; }
+
+
+        public Book(string _itemId, string _title, double _price)
+        {
+            ItemId = _itemId;
+            Title = _title;
+            Price = _price;
+        }
+        public Book(string _itemId, string _title):
+            this(_itemId, _title, 0)
+        {
+        }
+        public Book(string _itemId):
+            this(_itemId, string.Empty, 0)
+        {
+        }
+
+        public override string ToString()
+        {
+            NumberFormatInfo dotToComma = new NumberFormatInfo();
+            dotToComma.NumberDecimalSeparator = ",";
+            return "ItemId: " + ItemId + ", Title: " + Title + ", Price: " + Price.ToString(dotToComma);
+        }
+    }
+}
